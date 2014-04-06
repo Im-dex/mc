cccff7a4ddce36b636b3ecc23a94fcddc4e60efe
imdex <imdex@localhost>
2014-04-06 19:09:37 +0400

    Parser:  - operators refactoring  - fixed compilation

--- parser/src/main/scala/org/mc/parser/CupScanner.scala
+++ parser/src/main/scala/org/mc/parser/CupScanner.scala
@@ -10,46 +10,46 @@ import org.mc.lexer.IdToken
 
 final class CupScanner(val reader: StreamReader) extends Scanner
                                                  with Immutable {
-    val scanner = new Lexer(reader)
+    val scanner = new Lexer(reader.asInstanceOf[java.io.Reader])
 
     @throws(classOf[Exception])
     override def next_token(): Symbol = {
-        val lexToken = scanner.nextToken()
+        val token = scanner.nextToken()
 
         try {
-            convertToken(lexToken)
+            convertToken(token)
         }
         catch {
-            case SkipTokenException => next_token()
+            case _: SkipTokenException => next_token()
         }
     }
 
     @throws(classOf[SkipTokenException])
-    private def convertToken(token: Token): Symbol = {
-        lexToken match {
+    private def convertToken(scannerToken: Token): Symbol = {
+        scannerToken match {
             case token: IdToken        => new Symbol(sym.ID, token)
             case token: StringToken    => new Symbol(sym.STRING, token)
             case token: DecNumberToken => new Symbol(sym.DEC_NUMBER, token)
 
-            case EofToken              => new Symbol(sym.EOF)
-            case CommentToken          => throw SkipTokenException("Comment")
-            case ErrorToken            => new Symbol(sym.error)
+            case _: EofToken           => new Symbol(sym.EOF)
+            case _: CommentToken       => throw SkipTokenException("Comment")
+            case _: ErrorToken         => new Symbol(sym.error)
 
-            case KwValToken            => new Symbol(sym.VAL)
-            case KwVarToken            => new Symbol(sym.VAR)
+            case _: KwValToken         => new Symbol(sym.VAL)
+            case _: KwVarToken         => new Symbol(sym.VAR)
 
-            case SemicolonToken        => new Symbol(sym.SEMICOLON)
+            case _: SemicolonToken     => new Symbol(sym.SEMICOLON)
 
-            case AssignToken           => new Symbol(sym.ASSIGN)
-            case PlusToken             => new Symbol(sym.PLUS)
-            case MinusToken            => new Symbol(sym.MINUS)
-            case TimesToken            => new Symbol(sym.TIMES)
-            case DivideToken           => new Symbol(sym.DIVIDE)
+            case _: AssignToken        => new Symbol(sym.ASSIGN)
+            case _: PlusToken          => new Symbol(sym.PLUS)
+            case _: MinusToken         => new Symbol(sym.MINUS)
+            case _: TimesToken         => new Symbol(sym.TIMES)
+            case _: DivideToken        => new Symbol(sym.DIVIDE)
 
-            case LeftParenToken        => new Symbol(sym.LPAREN)
-            case RightParenToken       => new Symbol(sym.RPAREN)
+            case _: LeftParenToken     => new Symbol(sym.LPAREN)
+            case _: RightParenToken    => new Symbol(sym.RPAREN)
 
-            case _                     => throw InvalidArgumentException()
+            case _                     => new Symbol(sym.error)
         }
     }
 }

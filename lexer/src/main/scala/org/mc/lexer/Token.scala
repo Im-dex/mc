26e1e3603c08872617c30955933655a15828430e
imdex <tsukanov.anton@gmail.com>
2014-05-08 13:04:31 +0400

    - added new tokens  - refactoring

--- lexer/src/main/scala/org/mc/lexer/Token.scala
+++ lexer/src/main/scala/org/mc/lexer/Token.scala
@@ -6,25 +6,27 @@ sealed abstract class Token extends Immutable {
     val column: Int
 }
 
-case class IdToken(text: String, line: Int, column: Int, name: String) extends Token
-case class StringToken(text: String, line: Int, column: Int, value: String) extends Token
-case class DecNumberToken(text: String, line: Int, column: Int, value: BigInt) extends Token
+final case class IdToken(text: String, line: Int, column: Int, name: String) extends Token
+final case class StringToken(text: String, line: Int, column: Int, value: String) extends Token
+final case class DecNumberToken(text: String, line: Int, column: Int, value: BigInt) extends Token
 
-case class EofToken(text: String, line: Int, column: Int) extends Token
-case class CommentToken(text: String, line: Int, column: Int) extends Token
-case class ErrorToken(text: String, line: Int, column: Int) extends Token
+final case class EofToken(text: String, line: Int, column: Int) extends Token
+final case class CommentToken(text: String, line: Int, column: Int) extends Token
+final case class ErrorToken(text: String, line: Int, column: Int) extends Token
 
-case class KwValToken(text: String, line: Int, column: Int) extends Token
-case class KwVarToken(text: String, line: Int, column: Int) extends Token
+final case class KwValToken(text: String, line: Int, column: Int) extends Token
+final case class KwVarToken(text: String, line: Int, column: Int) extends Token
 
-case class SemicolonToken(text: String, line: Int, column: Int) extends Token
-case class ColonToken(text: String, line: Int, column: Int) extends Token
+final case class SemicolonToken(text: String, line: Int, column: Int) extends Token
+final case class ColonToken(text: String, line: Int, column: Int) extends Token
 
-case class AssignToken(text: String, line: Int, column: Int) extends Token
-case class PlusToken(text: String, line: Int, column: Int) extends Token
-case class MinusToken(text: String, line: Int, column: Int) extends Token
-case class TimesToken(text: String, line: Int, column: Int) extends Token
-case class DivideToken(text: String, line: Int, column: Int) extends Token
+final case class AssignToken(text: String, line: Int, column: Int) extends Token
+final case class PlusToken(text: String, line: Int, column: Int) extends Token
+final case class MinusToken(text: String, line: Int, column: Int) extends Token
+final case class TimesToken(text: String, line: Int, column: Int) extends Token
+final case class DivideToken(text: String, line: Int, column: Int) extends Token
 
-case class LeftParenToken(text: String, line: Int, column: Int) extends Token
-case class RightParenToken(text: String, line: Int, column: Int) extends Token
+final case class OpenParenToken(text: String, line: Int, column: Int) extends Token
+final case class CloseParenToken(text: String, line: Int, column: Int) extends Token
+final case class OpenCurlyBraceToken(text: String, line: Int, column: Int) extends Token
+final case class CloseCurlyBraceToken(text: String, line: Int, column: Int) extends Token

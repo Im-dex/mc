60678fc956cd8972b13073d312c895527ae3022f
a.tsukanov <tsukanov.anton@gmail.com>
2014-05-13 21:06:43 +0400

    type system refactoring: primitive types added

--- ir/src/main/scala/org/mc/ir/Operand.scala
+++ ir/src/main/scala/org/mc/ir/Operand.scala
@@ -1,6 +1,6 @@
 package org.mc.ir
 
-sealed abstract class Operand(valueType: Type)
+sealed abstract class Operand[T <: Type]
 
-final case class Symbol(valueType: Type) extends Operand(valueType)
-final case class Constant(valueType: Type) extends Operand(valueType)
+final case class Symbol[T <: Type]() extends Operand[T]
+final case class Constant[T <: PrimitiveType]() extends Operand[T]

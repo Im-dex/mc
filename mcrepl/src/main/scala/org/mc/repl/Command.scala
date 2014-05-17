022c358f1dc7e00ee27a6e0ce9e279c26ac57d9e
Imdex <Imdex@localhost>
2014-05-17 17:42:29 +0400

    - removed mcrepl project

--- mcrepl/src/main/scala/org/mc/repl/Command.scala
+++
@@ -1,7 +0,0 @@
-package org.mc.repl
-
-abstract class Command extends Immutable
-
-final case class Help() extends Command
-final case class Quit() extends Command
-final case class ExecCode(source: String) extends Command

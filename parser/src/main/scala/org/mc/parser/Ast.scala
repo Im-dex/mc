759b9118da0503965e763b9aa96ffca01424a688
Imdex <Imdex@localhost>
2014-06-12 00:19:06 +0400

    Parser:  - grammar update

--- parser/src/main/scala/org/mc/parser/Ast.scala
+++ parser/src/main/scala/org/mc/parser/Ast.scala
@@ -6,7 +6,18 @@ sealed abstract class Ast extends Symbol with Immutable
 
 object Ast {
     sealed abstract class Expression extends Ast with Immutable
-    //sealed abstract class Statement extends Ast with Immutable
+    sealed abstract class Statement extends Ast with Immutable
+    sealed abstract class Modifier extends Ast with Immutable
+
+    //=========================================================================================================
+    // module
+    //=========================================================================================================
+
+    final case class Module(statements: List[Statement]) extends Ast with Immutable
+
+    //=========================================================================================================
+    // expressions
+    //=========================================================================================================
 
     sealed abstract class BinaryExpression extends Expression with Immutable {
         val left: Expression
@@ -17,9 +28,10 @@ object Ast {
         val expression: Expression
     }
 
-    sealed abstract class Literal extends Expression with Immutable
+    sealed abstract class Literal extends Expression with Immutable {
+        val text: String
+    }
 
-    //=========================================================================================================
 
     final case class ExpressionList(expressions: List[Expression]) extends Ast with Immutable
     final case class EmptyExpression() extends Expression with Immutable
@@ -33,20 +45,39 @@ object Ast {
     final case class MinusExpression(expression: Expression) extends UnaryExpression with Immutable
     final case class PlusExpression(expression: Expression) extends UnaryExpression with Immutable
 
-    final case class IdLiteral() extends Literal with Immutable
-    final case class DecNumberLiteral() extends Literal with Immutable
-    final case class HexNumberLiteral() extends Literal with Immutable
-    final case class BinNumberLiteral() extends Literal with Immutable
-    final case class FloatLiteral() extends Literal with Immutable
-    final case class HexFloatLiteral() extends Literal with Immutable
-    final case class DoubleLiteral() extends Literal with Immutable
-    final case class HexDoubleLiteral() extends Literal with Immutable
-    final case class CharLiteral() extends Literal with Immutable
-    final case class StringLiteral() extends Literal with Immutable
+    final case class IdLiteral(text: String) extends Literal with Immutable
+    final case class DecNumberLiteral(text: String) extends Literal with Immutable
+    final case class HexNumberLiteral(text: String) extends Literal with Immutable
+    final case class BinNumberLiteral(text: String) extends Literal with Immutable
+    final case class FloatLiteral(text: String) extends Literal with Immutable
+    final case class HexFloatLiteral(text: String) extends Literal with Immutable
+    final case class DoubleLiteral(text: String) extends Literal with Immutable
+    final case class HexDoubleLiteral(text: String) extends Literal with Immutable
+    final case class CharLiteral(text: String) extends Literal with Immutable
+    final case class StringLiteral(text: String) extends Literal with Immutable
 
     //final case class ValueDefinition(token: IdToken, init: Ast) extends Ast
     //final case class VariableDefinition(token: IdToken, init: Ast) extends Ast
 
-    //final case class ClassDeclaration(name: String, modifiers: ClassModifiers, inheritanceInfo: ClassInheritanceInfo) extends Ast
-    //final case class InterfaceDeclaration(name: String, accessModifier: AccessModifier, baseInterfaces: List[String]) extends Ast
+    //=========================================================================================================
+    // statements
+    //=========================================================================================================
+
+    sealed abstract class TypeDeclaration extends Statement with Immutable
+
+    final case class ErrorStatement() extends Statement with Immutable
+
+    final case class ModuleStatement() extends Statement with Immutable
+    final case class ImportStatement() extends Statement with Immutable
+
+    final case class ClassDeclaration(name: String, modifiers: List[Modifier], baseClass: Option[String],
+                                      interfaces: List[String]) extends TypeDeclaration with Immutable
+
+    //=========================================================================================================
+    // modifiers
+    //=========================================================================================================
+
+    final case class Private() extends Modifier with Immutable
+    final case class Protected() extends Modifier with Immutable
+    final case class Final() extends Modifier with Immutable
 }
